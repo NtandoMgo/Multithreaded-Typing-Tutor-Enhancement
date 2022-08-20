dc9f9c50dbd62dca6896dd5444b18291c85b3083
Ntandoyabalele <lubelog.011@gmail.com>
2022-08-21 01:55:32 +0200

    Works fine when there are duplicates, catch the lowest, and resert the words when done

--- CatchWord.java
+++ CatchWord.java
@@ -15,7 +15,7 @@ public class CatchWord extends Thread {
 	CatchWord(String typedWord) {
 		target=typedWord;
 	}
-
+/* 
 	// make it catch a word lowest if there's a duplicate
 	public void Catch(FallingWord[] wordList, String typedWord) {
 		int track = 0;
@@ -30,7 +30,7 @@ public class CatchWord extends Thread {
 		if (track > 1){
 
 		}
-	}
+	}*/
 
 	public static void setWords(FallingWord[] wordList) {
 		words=wordList;	
@@ -47,9 +47,10 @@ public class CatchWord extends Thread {
 	}
 	
 	public void run() {
-		int i=0;
+		/*int i=0;
 		while (i<noWords) {		
 			while(pause.get()) {};
+
 			if (words[i].matchWord(target)) {
 				System.out.println( " score! '" + target); //for checking
 				score.caughtWord(target.length());	
@@ -57,7 +58,38 @@ public class CatchWord extends Thread {
 				break;
 			}
 		   i++;
+		}*/
+		/*for (int i = 0; i < noWords; i++) {
+			while(pause.get()) {};
+			if (words[i].matchWord(target)) {
+				System.out.println( " score! '" + target); //for checking
+				score.caughtWord(target.length());	
+				//FallingWord.increaseSpeed();
+				break;
+			}
+		}*/
+		//int k = 0;
+		int p = -1;
+		for (int j = 0; j < noWords; j++) {
+			while(pause.get()) {};	// will not execute whats inside till user press start
+
+			if (words[j].matchWord(target)) {		// condition = true if the typedWord is in words(falling words)
+				if (p < 0) {		// no duplicates 
+					p = j;
+				} else if (words[j].getY() > words[p].getY()){
+					//there are duplicates and keep the index position of the lowest words (greater height-y)
+					p = j;
+				}
+				//k++;
+				
+			}
 		}
-		
+		if (p < 0) {	// no duplicate
+			return;
+		}else{
+			System.out.println( " score! '" + target); //for checking
+			words[p].resetWord();
+			score.caughtWord(target.length());
+		}
 	}	
 }
